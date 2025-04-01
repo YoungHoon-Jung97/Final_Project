83fdb73ac1c1b4426e22898b7882b55a25b3ec35
YongHoon-Jung97 <sunmoon0906@naver.com>
2025-04-02 00:16:20 +0900

    add teamMain

--- project/src/com/project/mvc/TeamFormController.java
+++ project/src/com/project/mvc/TeamFormController.java
@@ -8,15 +8,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class TeamFormController
 {
 	//동호회 메이 페이지 호출
-	@RequestMapping(value="/teammainform.action", method = RequestMethod.GET)
-	public String teamMainForm() {
-		return "/team/TeamMainForm.jsp";
+	@RequestMapping(value="/teammain.action", method = RequestMethod.GET)
+	public String teamMain() {
+		return "/team/TeamMain.jsp";
 	}
 	
-	//투표 페이지 호출
-	@RequestMapping(value="/voteform.action", method = RequestMethod.GET)
-	public String voteForm() {
-		return "/team/VoteForm.jsp";
+	//일정 호출
+	@RequestMapping(value="/teamschedule.action", method = RequestMethod.GET)
+	public String teamSchedule() {
+		return "/team/TeamSchedule.jsp";
 	}
 	
+	//가게부 호출
+	@RequestMapping(value="/teamfee.action" , method = RequestMethod.GET)
+	public String teamFee() {
+		return "/team/TeamFee.jsp";
+	}
+	
+	//팀 게시판 호출
+	@RequestMapping(value="/teamboard.action" , method = RequestMethod.GET)
+	public String teamBoard() {
+		return "/team/TeamBoard.jsp";
+	}
 }
