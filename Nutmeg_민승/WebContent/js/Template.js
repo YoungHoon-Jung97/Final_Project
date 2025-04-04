c162246895cd13dc8f896544e8b9d137527e187e
kms7020 <msk7262@gmail.com>
2025-04-04 16:59:18 +0900

    add

---
+++ Nutmeg_민승/WebContent/js/Template.js
@@ -0,0 +1,98 @@
+/*
+	Template.js
+*/
+
+$(function()
+{
+	$(".menu-icon").click(function()
+	{
+		if ($(this).hasClass("active"))
+		{
+			$(this).removeClass("active").addClass("inactive");
+			$(".nav-menu").removeClass("active");
+			$(".nav-sub").hide();
+		}
+		
+		else
+		{
+			$(this).removeClass("inactive").addClass("active");
+			$(".nav-menu").addClass("active");
+			$(".nav-sub").show();
+		}
+	});
+	
+	$(".right-menu").on("click", function ()
+	{
+		if ($(this).hasClass("active"))
+		{
+			$(this).removeClass("active").addClass("inactive");
+			$(this).find(".user-icon").removeClass("shrink").addClass("inshrink");
+			
+			if (user_id != null)
+				$(".user-menu").hide();
+		}
+		
+		else
+		{
+			$(this).removeClass("inactive").addClass("active");
+			$(this).find(".user-icon").removeClass("inshrink").addClass("shrink");
+			
+			if (user_id != null)
+				$(".user-menu").show();
+		}
+	});
+	
+	$(".team").click(function()
+	{
+		window.location.href = "Team.action";
+	});
+	
+	$(".temp-team").click(function()
+	{
+		window.location.href = "TempTeam.action";
+	});
+	
+	$(".field").click(function()
+	{
+		window.location.href = "Field.action";
+	});
+	
+	$(".stadium").click(function()
+	{
+		window.location.href = "Stadium.action";
+	});
+	
+	$(".mercenary-offer").click(function()
+	{
+		window.location.href = "MercenaryOffer.action";
+	});
+	
+	$(".mercenary").click(function()
+	{
+		window.location.href = "Mercenary.action";
+	});
+	
+	$(".match").click(function()
+	{
+		window.location.href = "Match.action";
+	});
+	
+	$(".myInformation").click(function()
+	{
+		window.location.href = "MyInformation.action";
+	});
+	
+	$(".myTeam").click(function()
+	{
+		window.location.href = "MyTeam.action";
+	});
+	
+	$(".logout").click(function()
+	{
+		var currentPath = window.location.pathname.replace("/project", "");
+		var currentUrl = currentPath + window.location.search;
+		var returnUrl = currentUrl + (currentUrl.includes('?') ? '&' : '?') + "logoutMsg=1";
+		
+		window.location.href = "/project" + "/Logout.action?returnUrl=" + encodeURIComponent(returnUrl);
+	});
+});
